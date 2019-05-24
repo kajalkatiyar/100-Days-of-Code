da907b0b2508be16916b368cdbdeba006587fa7e
KAJAL KATIYAR <kajalkatiyar008@gmail.com>
2019-05-24 21:56:17 +0530

    Numpy

--- Day-7/arange.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-print("5X2 array using np.arange:")
-a=np.arange(100,200,10)
-a=a.reshape(5,2)
-print(a)
--- Day-7/ret3.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-a=np.array([[12,34,56],[1,2,3],[9,67,37],[1,4,7]])
-print(a)
-print("Third col.:")
-print(a[...,2])
